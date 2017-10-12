de93c8fffa6c458eee06894a076393db33246794
Americo Tomas <atomas@totallymoney.com>
2017-10-12 19:21:02 +0100

    SANDBOX: get modal module inside rc-tooltip module

--- snippets/src/App.js
+++ snippets/src/App.js
@@ -1,20 +1,95 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Tooltip from 'rc-tooltip';
+import Modal from 'react-modal';
+
 import './App.css';
 
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-45%',
+    transform             : 'translate(-50%, -50%)'
+  },
+    overlay: {
+      backgroundColor     : 'rgba(255, 0, 255, 0.75)'
+    }
+};
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false,
+      visible: false
+    };
+
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  onVisibleChange = (visible) => {
+    this.setState({
+      visible,
+    });
+  }
+
   render() {
+    if (this.state.destroy) {
+      return null;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div>
+      <div style={{ marginTop: 60 }}>
+        <Tooltip
+          visible={this.state.visible}
+          onVisibleChange={this.onVisibleChange}
+          trigger="click"
+          destroyTooltipOnHide
+          overlay={
+            <div className="modal">
+              <Modal
+                isOpen={true}
+                onAfterOpen={this.afterOpenModal}
+                onRequestClose={this.closeModal}
+                style={customStyles}
+                contentLabel="Example Modal"
+              >
+      
+                <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+                <button onClick={this.closeModal}>close</button>
+                <div>I am a modal</div>
+                <form>
+                  <input />
+                  <button>tab navigation</button>
+                  <button>stays</button>
+                  <button>inside</button>
+                  <button>the modal</button>
+                </form>
+              </Modal>
+            </div>
+          }
+        >
+          <a href="#" onClick={preventDefault} style={{ color: '#fff', marginLeft: 10 }}>trigger</a>
+        </Tooltip>
       </div>
-    );
+    </div>);
   }
 }
 
